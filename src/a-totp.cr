11f994fbfa0ba4f4c5610f0ecc4b59b3aee891bb
iganeshk <kumar@iganesh.com>
2019-10-14 01:01:32 -0400

    add icon support

--- src/a-totp.cr
+++ src/a-totp.cr
@@ -1,7 +1,7 @@
 require "json"
 
 module ATotp
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
   ENV["PATH"] = "/usr/local/bin:/usr/bin"
 
   # This could possibly be cached.
@@ -26,22 +26,30 @@ module ATotp
     ids.map do |id|
       pass = get_pass id
       code = gen_code pass
+      workflow_path = Dir.current
+      if File.exists?("#{workflow_path}/icons/#{id}.png")
+        alfred_icon = "#{workflow_path}/icons/#{id}.png"
+      else
+        alfred_icon = "#{workflow_path}/icon.png"
+      end
       {
-        uid: id,
-        title: id,
+        uid:      id,
+        title:    id,
         subtitle: code,
-        arg: pass,
+        arg:      pass,
+        icon:     {
+          path: "#{alfred_icon}",
+        },
         mods: {
           alt: {
-            valid: true,
+            valid:    true,
             subtitle: "copy #{code}",
-            arg: pass,
-          }
-        }
+            arg:      pass,
+          },
+        },
       }
     end
   end
 
-  print({ rerun: 1, items: alfred_out }.to_json)
+  print({rerun: 1, items: alfred_out}.to_json)
 end
-
